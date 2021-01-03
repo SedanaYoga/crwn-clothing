8665115290197cfc48b9a290de4436078d2dd993
SedanaYoga <9g.yoga27@gmail.com>
2021-01-03 17:17:40 +0700

    Updating app to redirect to home if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { UserActionTypes } from "./user.types";
+
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER", //HARUS SAMA DENGAN DI REDUCER
+  type: UserActionTypes.SET_CURRENT_USER, //HARUS SAMA DENGAN DI REDUCER
   payload: user,
 });
